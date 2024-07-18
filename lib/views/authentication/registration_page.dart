fe61abef2c01607c7aae1972793f176c8ec4d401
jhill144 <jhill144@student.gsu.edu>
2024-07-18 12:29:52 -0400

    Revert "Added fields to the registration page"
    This reverts commit 610e1a59e4600740ae1a01ad0a4cd24de2fac9a0.

--- lib/views/authentication/registration_page.dart
+++ lib/views/authentication/registration_page.dart
@@ -1,118 +1,13 @@
-import 'package:artgallery/utilities/directoryrouter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
-import 'package:go_router/go_router.dart';
 
 class RegistrationPage extends StatelessWidget {
-  RegistrationPage({super.key});
-  final _formKey = GlobalKey<FormBuilderState>();
-  final _fnameFieldKey = GlobalKey<FormBuilderFieldState>();
-  final _lnameFieldKey = GlobalKey<FormBuilderFieldState>();
-  final _emailFieldKey = GlobalKey<FormBuilderFieldState>();
-  final _passwordFieldKey = GlobalKey<FormBuilderFieldState>();
+  const RegistrationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
-              colors: [Colors.blue, Colors.green],
-            ),
-          ),
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.art_track,
-                size: 50,
-              ),
-              const Text(
-                'Art Gallery',
-                style: TextStyle(fontSize: 30),
-              ),
-              const SizedBox(height: 10),
-              FormBuilder(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    FormBuilderTextField(
-                      key: _fnameFieldKey,
-                      name: 'fnameField',
-                      decoration: const InputDecoration(
-                          labelText: 'First Name',
-                          hintText: 'Enter your First Name.'),
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                      ]),
-                    ),
-                    const SizedBox(height: 10),
-                    FormBuilderTextField(
-                      key: _lnameFieldKey,
-                      name: 'lnameField',
-                      decoration: const InputDecoration(
-                          labelText: 'Last Name',
-                          hintText: 'Enter your Last Name.'),
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                      ]),
-                    ),
-                    const SizedBox(height: 10),
-                    FormBuilderTextField(
-                      key: _emailFieldKey,
-                      name: 'emailField',
-                      decoration: const InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Enter your email address.'),
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                        FormBuilderValidators.email(),
-                      ]),
-                    ),
-                    const SizedBox(height: 10),
-                    FormBuilderTextField(
-                      key: _passwordFieldKey,
-                      name: 'password',
-                      decoration: const InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter your password.'),
-                      obscureText: true,
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                      ]),
-                    ),
-                    const SizedBox(height: 10),
-                    MaterialButton(
-                      color: Theme.of(context).colorScheme.primary,
-                      onPressed: () {
-                        // Validate and save the form values
-                        _formKey.currentState?.saveAndValidate();
-                        debugPrint(_formKey.currentState?.value.toString());
-
-                        // On another side, can access all field values without saving form with instantValues
-                        _formKey.currentState?.validate();
-                        debugPrint(
-                            _formKey.currentState?.instantValue.toString());
-                      },
-                      child: const Text('Create Account'),
-                    ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                        onPressed: () {
-                          context.goNamed(DirectoryRouter.loginpage);
-                        },
-                        child: const Text('Already Registered? Login.')),
-                  ],
-                ),
-              ),
-            ],
-          )),
-        ),
+        child: Text('Registration'),
       ),
     );
   }
